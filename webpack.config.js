5a87e262a71851c26f68ca19dc8fdccbb6a72418
Severin Sverdvik <severisv@gmail.com>
2017-10-19 16:06:16 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { CheckerPlugin } = require("awesome-typescript-loader");
+
+module.exports = {
+    entry: {
+        'reactapp': [
+          'babel-polyfill',
+          'react-hot-loader/patch',
+          './index.tsx'
+        ]
+      },
+
+    output: {
+        path: path.resolve("../dist"),
+        filename: "[name].js",
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
+        modules: [path.resolve("./node_modules"), path.resolve("./")],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader",
+                exclude: /node_modules/,
+            },
+            { test: /\.(jpg|png|svg)$/, loader: "url-loader" },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: true },
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            sourceMap: true,
+                            paths: [path.join(__dirname, "static")],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+            },
+        ],
+    },
+    plugins: [new CheckerPlugin()],
+};
